1a4b430f3b4c8e219d38f0d1a07d9e0f46460178
Isaac Dettman <isaac.dettman@nbcuni.com>
2017-04-14 14:33:38 -0700

    edit html with flexbox list

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('index.js loaded')
+'use strict'
+
+{
+	const documentResize = () => {
+		console.log('resize:', window.innerWidth, window.innerHeight)
+	}
+	
+	const documentReady = () => {
+		if (document.readyState === 'ready') {
+			window.removeEventListener('load', documentReady)
+			window.addEventListener('resize', documentResize)
+		}
+	}
+	
+	window.addEventListener('load', documentReady)
+}
