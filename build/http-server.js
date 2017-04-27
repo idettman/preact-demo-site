21ec5b5988185d01ab76bfc07bb095605e47713f
Isaac Dettman <isaac.dettman@nbcuni.com>
2017-04-27 13:58:27 -0700

    update directory structure for development http server

--- build/http-server.js
+++ build/http-server.js
@@ -3,10 +3,10 @@ const url = require('url')
 const fs = require('fs')
 
 http.createServer((req, response) => {
+	
 	try {
 		const requestUrl = url.parse(req.url)
 		const filename = (requestUrl.pathname.indexOf('.') === -1) ? 'index.html' : requestUrl.pathname.substring(1)
-		
 		response.writeHead(200)
 		fs.createReadStream(filename).pipe(response)
 	}
