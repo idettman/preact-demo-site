198a5c7d477e96d98541acf4791123ea8e1b4a6e
Isaac Dettman <isaac.dettman@nbcuni.com>
2017-04-27 10:26:51 -0700

    add js util methods

---
+++ js/util.js
@@ -0,0 +1,33 @@
+/**
+ * @param a
+ * @param b
+ */
+const pipe = (a, b) => (c) => b(a(c))
+
+/**
+ * @param a
+ * @param b
+ */
+const compose = (a, b) => (c) => a(b(c))
+
+/**
+ * @type {Element}
+ */
+const lockedAspectContainer = document.body
+
+const documentResize = () => {
+	const w = (window.innerWidth)
+	lockedAspectContainer.style.height = w + 'px'
+}
+
+const processAfterFrame = command => window.requestAnimationFrame(command)
+
+const addResizeListener = () => window.addEventListener('resize', documentResize)
+
+const documentReady = () => {
+	if (document.readyState === 'complete') {
+		window.removeEventListener('load', documentReady)
+		documentResize()
+		processAfterFrame(addResizeListener)
+	}
+}
