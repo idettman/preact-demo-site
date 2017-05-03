7a2eddab056d6eb8f67456ed61e4ab76d9e13164
Isaac Dettman <isaac.dettman@nbcuni.com>
2017-05-03 15:39:24 -0700

    update changed title text with svg for better gradient text support

--- js/components/header.js
+++ js/components/header.js
@@ -1,7 +1,15 @@
 import {h} from 'preact'
 
 const Header = (props) => (
-	<h1 class="header__title">{ props.title }</h1>
+	<svg class="header__title" viewBox="0 0 36 9">
+		<defs>
+			<linearGradient id="MyGradient" x1="0%" x2="0%" y1="0%" y2="100%">
+				<stop offset="0%" stop-color="cornflowerblue"/>
+				<stop offset="100%" stop-color="#0B598D"/>
+			</linearGradient>
+		</defs>
+		<text x="0" y="7" style="fill: url(#MyGradient); font-size: 1rem">{ props.title }</text>
+	</svg>
 )
 
 export default Header
