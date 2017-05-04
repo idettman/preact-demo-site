8cef12c6f6719c48d72a3133601d6232f5ccb289
Isaac Dettman <isaac.dettman@nbcuni.com>
2017-05-04 16:00:51 -0700

    update content on repos page with addition repos

--- js/views/repos.js
+++ js/views/repos.js
@@ -4,9 +4,21 @@ class Repos extends Component {
 	render() {
 		return (
 			<div>
-				<h1>Repos content</h1>
+				<h1>Repos</h1>
 				<dl>
-					<dt>storygraph</dt>
+					<dt>this site's source</dt>
+					<dd><a href="https://github.com/idettman/idettman.github.io">idettman.github.io</a></dd>
+					
+					<dt>preact project template</dt>
+					<dd><a href="https://github.com/idettman/preact-project-template">preact-project-template</a></dd>
+					
+					<dt>preact + matter.js project template</dt>
+					<dd><a href="https://github.com/idettman/preact-matter-template">preact-matter-template</a></dd>
+					
+					<dt>minimal react project template</dt>
+					<dd><a href="https://github.com/idettman/react-app-template">react-app-template</a></dd>
+					
+					<dt>javascript story graph engine</dt>
 					<dd><a href="https://github.com/idettman/story-graph">story-graph</a></dd>
 					
 					<dt>webidl-externs</dt>
